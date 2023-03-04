ba0d6e944d12b8041cd7ebacaf771ad738333170
zohde <zohdetamimi07@gmail.com>
2023-03-04 23:55:20 +0300

    hel

--- backend/task/read.php
+++ backend/task/read.php
@@ -1,39 +1,38 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin:*");
     header("Content-Type: application/json;");
+    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
-    include_once './config.php';
-    include_once './users.php';
+    include_once '../config.php';
+    // include_once './users.php';
 
     $database = new DB();
     $db = $database->getConnection();
+    $data = json_decode(file_get_contents("php://input"));
+    $user_id = $data->user_id;
+    $categoryName = $data->categoryName;
+    $status =$data->status;
+    // print_r($data);
+    // print_r(json_encode($tasks));
 
-    $users = new User($db);
-
-    $stmt = $users->getUsers();
-    $itemCount = $stmt->rowCount();
-
-    if($itemCount > 0){
-        
-        $userArr = array();
-       
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            extract($row);
-            $e = array(
-                "id" => $id,
-                "user_name" => $user_name,
-                // "last_name" => $last_name,
-                "password" => $password,
-                "photo" => $photo,
-                "email" => $email
-            );
-
-            array_push($userArr, $e);
-        }
-        echo json_encode($userArr);
+    $sql = "SELECT * FROM tasks WHERE tasks.user_id = $user_id";
+    if( $status &&  $categoryName){
+        $sql .= " AND completed = '$status' AND category_id = $categoryName";
+        // echo "category $categoryName , status $status";
     }
-    else{
-        echo json_encode('');
+    if( $status && !$categoryName){
+        $sql .= " AND completed = '$status'";
+        // echo "category $categoryName , status $status";
     }
-
+    if($categoryName && !$status){
+        $sql .= " AND category_id = $categoryName";
+        // echo "category $categoryName , status $status";
+    }
+    $stmt = $db->prepare($sql);
+    // $stmt->bindParam(':user_id', $user_id);
+    $stmt->execute();
+    $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r(json_encode($tasks));
 ?>
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -8,20 +8,40 @@ function Tasks() {
         priority : "",
         completed :""
     });
+    const [filter, setFilter] = useState({
+        user_id : JSON.parse(localStorage.getItem("user")).id,
+        status: '',
+        categoryName: ''
+    })
     const [category , setcategory] = useState([]);
+    const [tasks , setTasks] = useState([]);
+    const id = JSON.parse(localStorage.getItem("user")).id
     console.log(JSON.parse(localStorage.getItem("user")).id)
     useEffect(()=>{
         axios.get("http://localhost/taskmanger/backend/category/read.php").then(res=>{
-            console.log(res);
+            // console.log(res);
             setcategory(res.data)
         });
-
-    
+        axios.post("http://localhost/taskmanger/backend/task/read.php", filter).then(res=>{
+            console.log(res);
+            setTasks(res.data)
+        });
     },[])
+    const handleFilterChange = (e)=>{
+        setFilter(prev=>({...prev,[e.target.name]:e.target.value}))
+    }
+    const handleClick = ()=>{
+        console.log(filter)
+        axios.post("http://localhost/taskmanger/backend/task/read.php", filter).then(res=>{
+            console.log(res);
+            setTasks(res.data)
+        });
+    }
+    console.log(filter)
     const handleChange = (e) => {
         setdata(prev=>({...prev,[e.target.name]:e.target.value}))
       }
-      console.log(data)
+    //   console.log(data)
       const handleSubmit =  (e) =>{
         e.preventDefault();
         const formData = new FormData()
@@ -51,38 +71,104 @@ function Tasks() {
     
         axios.post("http://localhost/taskmanger/backend/task/create.php",formData).then(res=>{
         console.log(res);
+        axios.post("http://localhost/taskmanger/backend/task/read.php", {user_id: id}).then(res=>{
+            console.log(res);
+            setTasks(res.data)
+        });
+        setdata({
+            name:"",
+            category : "",
+            priority : "",
+            completed :""
+        })
       });
       }
   return (
     <div>
-    <form onSubmit={handleSubmit}>
-    <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-        <input onChange={handleChange} value={data.name} name="name" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            <h1>create new task</h1>
+        <form onSubmit={handleSubmit} style={ {width:'400px', margin: '0 auto'}}>
+        <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+            <input onChange={handleChange} value={data.name} name="name" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="pet-select">Choose a Category:</label>
+            <select className="form-select"  onChange={handleChange} name="category" id="pet-select">
+                
+                {category.map((e) => (<option key={e.id} value={e.id}>{e.name}</option>))}
+            </select>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="pet-select">Choose a Priority:</label>
+            <select className="form-select"  onChange={handleChange} name="priority" id="pet-select">
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="pet-select">Choose a States:</label>
+            <select className="form-select"  onChange={handleChange} name="completed" id="pet-select">
+                <option value="completed">completed</option>
+                <option value="incomplete">incomplete</option>
+                
+            </select>
+        </div>
+            <button  type="submit" className="btn btn-primary">Submit</button>
+        </form>
+
+        {/* filter */}
+        <div className="container">
+            <div className="row">
+                <div className="mb-3 col">
+                    <label htmlFor="pet-select">Choose a States:</label>
+                    <select className="form-select"  onChange={handleFilterChange} name="status" id="pet-select">
+                        <option value="completed">completed</option>
+                        <option value="incomplete">incomplete</option>
+                        
+                    </select>
+                </div>
+                <div className="mb-3 col">
+                    <label htmlFor="pet-select">filter a Category:</label>
+                    <select className="form-select"  onChange={handleFilterChange} name="categoryName" id="pet-select">
+                        
+                        {category.map((e) => (<option key={e.id} value={e.id}>{e.name}</option>))}
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div className="show" style={{border: '1px solid'}}>
+            
+            {filter.categoryName && <div>filter base on category </div>}
+            {filter.status && <div>filter base on status </div>}
+            <button className='btn btn-dark' onClick={handleClick}>filter</button>
+        </div>
+        {/* end filter */}
+
+            <h2>Incomplete tasks</h2>
+        <table className="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">name</th>
+                <th scope="col">completed</th>
+                <th scope="col">priority</th>
+                <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks && tasks.map((e, i) =>(
+                    <tr className={e.priority == 'high' ? "table-danger" : e.priority == 'medium'  ? "table-warning" : ''} key={e.id}>
+                    <th scope="row">{i + 1}</th>
+                    <td>{e.name}</td>
+                    <td>{e.completed}</td>
+                    <td>{e.priority}</td>
+                    <td> <button>edit</button> <button>delete</button> </td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
     </div>
-    <label htmlFor="pet-select">Choose a Category:</label>
-    <select onChange={handleChange} name="category" id="pet-select">
-        
-        {category.map((e) => (<option key={e.id} value={e.id}>{e.name}</option>))}
-    </select>
-    <label htmlFor="pet-select">Choose a Priority:</label>
-    <select onChange={handleChange} name="priority" id="pet-select">
-        <option value="low">Low</option>
-        <option value="medium">Medium</option>
-        <option value="high">High</option>
-    </select>
-    <label htmlFor="pet-select">Choose a States:</label>
-    <select onChange={handleChange} name="completed" id="pet-select">
-        <option value="completed">completed</option>
-        <option value="incomplete">incomplete</option>
-        
-    </select>
-
-
-        <button  type="submit" className="btn btn-primary">Submit</button>
-
-    </form>
-</div>
 )
 }
 
